5747a1595d8c0d7c522c9bf63bec5cb01fc82b16
Eder Magalhaes <edermag@gmail.com>
2015-05-01 23:21:58 -0300

    build with gradle

--- run.sh
+++ run.sh
@@ -1,1 +1,18 @@
-mvn compile exec:java -Dexec.mainClass=br.com.yaw.mongo.MercadoriaController
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Build via maven ..."
+    mvn compile exec:java -Dexec.mainClass=br.com.yaw.mongo.MercadoriaController
+else
+    if [ $1 = "gradle" ]
+      then
+        echo "Build via gradle ..."
+        gradle run
+    else
+        echo "Build via maven ..."
+        mvn compile exec:java -Dexec.mainClass=br.com.yaw.mongo.MercadoriaController
+    fi
+fi
+
+
